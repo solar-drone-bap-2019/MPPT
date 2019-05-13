69581e3b6a2a742e02fc8207b4d8726bc5deac73
tomwemelsfelder <tomwemel@hotmail.com>
2019-05-13 15:40:15 +0200

    Update P_O_MPPT.ino
    Changed to use MPPT class

--- P_O_MPPT.ino
+++ P_O_MPPT.ino
@@ -1,71 +1,52 @@
+#include <Arduino.h>
+#include <MPPT.h>
+
 //current and voltage sensors
-#define CURRENT_1 PA0 
-#define VOLTAGE_1 PA1
-#define CURRENT_2 PA2
-#define VOLTAGE_2 PA3
+#define I1 PA0 
+#define V1 PA1
+#define I2 PA2
+#define V2 PA3
 
-//PWM pins for DC-DC converter switches
-#define PWM_1      PA8
-#define PWM_2      PA9
+//PWM pins for MPPT DC-DC converter switches
+#define PWM1      PA8
+#define PWM2      PA9
 
-//set up software counter to reduce effective clock speed
-int counter_lim = 720000;
-int counter;
+float perturbation = 0.01; // the change in the duty cycle
 
-
-float Perturbation = 0.01; // the change in the duty cycle
-
-float CURRENT_ARRAY[2] = {CURRENT_1, CURRENT_2};
-float VOLTAGE_ARRAY[2] = {VOLTAGE_1, VOLTAGE_2};
-float PWM_ARRAY[2] = {PWM_1, PWM_2};
-float P_prev_ARRAY[2];
-float Perturbation_ARRAY[2] = {Perturbation,Perturbation};
-float Duty_ARRAY[2];
+  MPPT MPPT1(I1,V1,PWM1);
+  MPPT MPPT2(I2,V2,PWM2);
 
 //declare variables for current, voltage and power
 float I,V,P;
 
 
 void setup()
-{
-  for (int N=0;N < sizeof(CURRENT_ARRAY);N++) { // for all MPPT groups
-    pinMode(CURRENT_ARRAY[N], INPUT); // set current sensor as input
-    pinMode(VOLTAGE_ARRAY[N], INPUT); // set voltage sensor as input
-    pinMode(PWM_ARRAY[N], OUTPUT); // set PWM signal as output
-  }
+{  
 }
 
 void loop()
-{
-  
-counter += 1;
-if (counter == counter_lim){ 
-  MPPT(1);
-  MPPT(2);
-  
-  counter = 0;
-}
-
+{  
+  P_O(MPPT1);
+  P_O(MPPT2);
+  delay(100);
 }
 
 //Track maximum power point using the 'Perturb and Observe' algorithm
-void MPPT(int N) // N = number of MPPT group
+void P_O(MPPT &mppt) 
 {  
   //observe
-  I = analogRead(CURRENT_ARRAY[N]); // read current sensor
-  V = analogRead(VOLTAGE_ARRAY[N]); // read voltage sensor
+  I = mppt.ReadI(); // read current sensor
+  V = mppt.ReadV(); // read voltage sensor
   P = I*V; // calculate power
 
   //perturb
-  if (P < P_prev_ARRAY[N]) { // if previous perturbation resulted in loss of power
-    Perturbation_ARRAY[N] = -Perturbation_ARRAY[N]; // reverse perturbation direction
+  if (P < mppt.Pprev) { // if previous perturbation resulted in loss of power
+    mppt.perturbDir = -mppt.perturbDir; // reverse perturbation direction
   }
-  Duty_ARRAY[N] = constrain(Duty_ARRAY[N]+Perturbation_ARRAY[N],0,1); // Apply perturbation as long as duty cycle stays in between 0 and 1
-  analogWrite(PWM_ARRAY[N],Duty_ARRAY[N]*255); // write PWM
-
+  mppt.dutyCycle = constrain(mppt.dutyCycle+perturbation*mppt.perturbDir,0,1); // Apply perturbation as long as duty cycle stays in between 0 and 1
+  mppt.WritePWM(); // write PWM
   
-  P_prev_ARRAY[N] = P; // write calculated power in array
+  mppt.Pprev = P; // store calculated power
 }
  
-
 
