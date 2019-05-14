4f931673fc5f06f14ca99c44fad994fd0fb91595
tomwemelsfelder <tomwemel@hotmail.com>
2019-05-14 17:54:00 +0200

    Update main.cpp
    added more comments

--- main.cpp
+++ main.cpp
@@ -1,19 +1,29 @@
 #include "mbed.h"
 #include "MPPT.h"
 
-#define I1 PA_0
-#define V1 PA_1
-#define I2 PA_2
-#define V2 PA_3
-#define PWM1 PA_8
-#define PWM2 PA_9
+/*
+Tracks the maximum power point for multiple solar arrays (solar cell groups).
+*/
 
-MPPT MPPT1(I1,V1,PWM1);
-MPPT MPPT2(I2,V2,PWM2);
+// Pins for tracking group 1
+#define I1 PA_0 // Current sensor
+#define V1 PA_1 // Voltage sensor
+#define PWM1 PA_8 // Pulse width modulation output
+
+// Pins for tracking group 2
+#define I2 PA_2 // Current sensor
+#define V2 PA_3 // Voltage sensor
+#define PWM2 PA_9 // Pulse width modulation output
+
+MPPT MPPT1(I1,V1,PWM1); // Create Maximum Power Point Tracker 1
+MPPT MPPT2(I2,V2,PWM2); // Create Maximum Power Point Tracker 2
 
 int main() {
     while(1) {
-        MPPT1.PerturbObserve();
-        MPPT2.PerturbObserve();
+        MPPT1.PerturbObserve(); // Execute the P&O algorithm for tracking group 1
+        MPPT2.PerturbObserve(); // Execute the P&O algorithm for tracking group 2
+
+        //MPPT doesn't need to happen very fast    
+        wait_ms(10); // Track the power point every 10 ms
     }
 }
