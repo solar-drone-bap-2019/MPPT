e50c4e8972b97b88dac5a0fcbe7fc133d2145db6
tomwemelsfelder <tomwemel@hotmail.com>
2019-05-14 14:50:03 +0200

    Update MPPT.h
    Changed voor programming in mbed.
    Now able to change PWM frequency

--- MPPT.h
+++ MPPT.h
@@ -1,41 +1,58 @@
- #include <Arduino.h>
+#include "mbed.h"
 
-class MPPT {
+float I, V, P;
+
+class MPPT{
   private:
-    int I_PIN; // current sensor pin
-    int V_PIN; // voltage sensor pin
-    int PWM_PIN; // pwm output pin
-  public:
-    MPPT(int I_pin, int V_pin, int PWM_pin);
-  
-    float Pprev; // previously measured power
-    float perturbDir; // perturbation direction
-    float dutyCycle; // PWM duty cycle
+    AnalogIn *CurrentSensor;
+    AnalogIn *VoltageSensor;
+    PwmOut *PwmOutput;
+    float PreviousPower;
+    float DutyCycle;
+    float Perturbation;
 
-    float ReadI();
-    float ReadV();
-    void WritePWM();
+  public:
+    MPPT(PinName I_pin,PinName V_pin,PinName PWM_pin);
+    float readI();
+    float readV();
+    void PerturbObserve();
 };
 
-  MPPT::MPPT(int I_pin,int V_pin,int PWM_pin){
-    I_PIN = I_pin;
-    V_PIN = V_pin;
-    PWM_PIN = PWM_pin;
+MPPT::MPPT(PinName I_pin,PinName V_pin,PinName PWM_pin){
+  AnalogIn Iin(I_pin);
+  AnalogIn Vin(V_pin);
+  PwmOut PWMout(PWM_pin);
+  CurrentSensor = &Iin;
+  VoltageSensor = &Vin;
+  PwmOutput = &PWMout;
+  Perturbation = 0.01;// the change in the duty cycle
+  PwmOutput->period_us(100); // set PWM frequency to 10kHz (1/100us = 10kHz)
+};
 
-    pinMode(I_pin,INPUT);
-    pinMode(V_pin,INPUT);
-    pinMode(PWM_pin,OUTPUT);
+float MPPT::readI(){
+  return CurrentSensor->read();
+}
 
+float MPPT::readV(){
+  return VoltageSensor->read();
+}
+
+void MPPT::PerturbObserve(){
+  //observe
+  I = CurrentSensor->read(); // read current sensor
+  V = VoltageSensor->read(); // read voltage sensor
+  P = I*V; // calculate power
+
+  //perturb
+  if (P < PreviousPower) { // if previous perturbation resulted in loss of power
+    Perturbation = -Perturbation; // reverse perturbation direction
   }
-
-  float MPPT::ReadI(){
-    return analogRead(I_PIN);
-  }
-
-  float MPPT::ReadV(){
-    return analogRead(V_PIN);
-  }
-
-  void MPPT::WritePWM(){
-    analogWrite(PWM_PIN,dutyCycle);
-  }
+  DutyCycle = DutyCycle+Perturbation; // Apply Perturbation
+  // Constrain duty cycle to be in between 0 and 1
+  if (DutyCycle < 0) {DutyCycle = 0;}
+  else if (DutyCycle > 1){DutyCycle = 1;}
+  
+  PwmOutput->write(DutyCycle); // write PWM
+  
+  PreviousPower = P; // store calculated power  
+}
