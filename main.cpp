5c5706bdb50247ad61ebf53fe0304996b73c9db0
tomwemelsfelder <tomwemel@hotmail.com>
2019-05-16 12:10:03 +0200

    Update main.cpp
    Added Charge Controller implementation, including options to track MPP, pause the MPPT, or track a lower power point

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include "mbed.h"
 #include "MPPT.h"
+#include "ChargeController.h"
 
 /*
-Tracks the maximum power point for multiple solar arrays (solar cell groups).
+Tracks the maximum power point for multiple solar arrays (solar cell groups),
+And controls the power flow in the system, to maximize solar power usage, without overcharging the batteries.
 */
+
+/* Control signal to be read from ChargeController.
+0 = track MPPT
+1 = pause MPPT
+2 = track Pload
+*/
+int Control; 
+
+float Target; // Target variable to be sent to MPPTs in case they need to track a lower power point
+
+/* All pins still need to be verified */
 
 // Pins for tracking group 1
 #define I1 PA_0 // Current sensor
@@ -18,12 +31,27 @@ Tracks the maximum power point for multiple solar arrays (solar cell groups).
 MPPT MPPT1(I1,V1,PWM1); // Create Maximum Power Point Tracker 1
 MPPT MPPT2(I2,V2,PWM2); // Create Maximum Power Point Tracker 2
 
+ChargeController CC(PA_4,PA_5,PA_6,PA_7); // Create Charge Controller object
+
 int main() {
     while(1) {
-        MPPT1.PerturbObserve(); // Execute the P&O algorithm for tracking group 1
-        MPPT2.PerturbObserve(); // Execute the P&O algorithm for tracking group 2
+        CC.run();
+        Control = CC.readControl();
+        if (Control == 0) { // Track MPPT
+            MPPT1.PerturbObserve(); // Execute the P&O algorithm for tracking group 1
+            MPPT2.PerturbObserve(); // Execute the P&O algorithm for tracking group 2
+        }
+        else if (Control == 1) { // Pause the MPPT algorithm    
+            MPPT1.pause(); 
+            MPPT2.pause();
+        }
+        else if (Control == 2) { // Track Target power
+            Target = CC.readTarget(); // read target variable.
+            MPPT1.PerturbObserve(Target);
+            MPPT2.PerturbObserve(Target);
+        }      
 
-        //MPPT doesn't need to happen very fast    
+        // MPPT doesn't need to happen very fast   
         wait_ms(10); // Track the power point every 10 ms
     }
 }
